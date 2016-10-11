d04f9587527f274d99e3f30e690e7b0c10084971
Chu <cnpanic@naver.com>
2016-10-11 15:32:50 +0900

    1011

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'main/index'
+  get 'main/404'
+  get 'main/contact'
+  get 'main/board'
+  get 'main/index2'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
